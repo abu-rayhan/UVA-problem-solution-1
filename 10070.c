48bb8ad4b1241bfe6de3b61ed94cc9de4c5bcc34
abu_rayhan <45509529+abu-rayhan@users.noreply.github.com>
2020-07-30 23:41:22 +0600

    10070.cpp

--- 10070.c
+++ 10070.c
@@ -1,53 +1,32 @@
-#include<stdio.h>
-#include<string.h>
-
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define ll long long
+#define pb push_back
+#define pi acos(-1)
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+ll yes(string s,ll p){
+    ll n=0;
+    for(ll i=0;i<s.size();i++){
+        n=n*10+s[i]-'0';
+        n=n%p;
+    }
+    if(n)return 0;
+    else return 1;
+}
 int main(){
-    long long n,l,d4,d1,d40,d15,d5,c,r,i,d,z=0;
-    char s[100000];
-    while(scanf("%s",s)!=EOF){
-        if(z==1)printf("\n");
-        l=strlen(s);
-        r=0;
-        for(i=0;i<l;i++){
-            r=r*10+s[i]-'0';
-            r=r%4;
-        }
-        d4=r;
-        r=0;
-        for(i=0;i<l;i++){
-            r=r*10+s[i]-'0';
-            r=r%100;
-        }
-        d1=r;
-        r=0;
-        for(i=0;i<l;i++){
-            r=r*10+s[i]-'0';
-            r=r%400;
-        }
-        d40=r;
-        r=0;
-        for(i=0;i<l;i++){
-            r=r*10+s[i]-'0';
-            r=r%15;
-        }
-        d15=r;
-        r=0;
-        for(i=0;i<l;i++){
-            r=r*10+s[i]-'0';
-            r=r%55;
-        }
-        d5=r;
-        c=1;
-        d=0;
-        if((d4==0&&d1!=0)||d40==0){
-            printf("This is leap year.\n");
-            z=1;
-            c=0;
-            if(d5==0){c=0;d=1;}
-        }
-        if(d15==0){printf("This is huluculu festival year.\n");c=0;z=1;}
-        if(d==1){printf("This is bulukulu festival year.\n");z=1;}
-        if(c==1){printf("This is an ordinary year.\n");z=1;}
+    string s;
+    ll k=0;
+    while(cin>>s){
+        if(k)cout<<endl;
+        ll f,h,fh,fi,ff,z=0,u=0;
+        f=yes(s,4),h=yes(s,100),fh=yes(s,400),fi=yes(s,15),ff=yes(s,55);
+        if((f&&!h)||fh){cout<<"This is leap year."<<endl;z=1;u=1;}
+        if(fi){cout<<"This is huluculu festival year."<<endl;u=1;}
+        if(z&&ff){cout<<"This is bulukulu festival year."<<endl;u=1;}
+        if(!u)cout<<"This is an ordinary year."<<endl;
+        k=1;
     }
     return 0;
 }
